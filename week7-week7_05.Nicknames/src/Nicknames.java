7b29cdc987ed37544c71dd2d9774abad6d259033
cpflyingduck <prashanta@Prashantas-MBP.home>
2015-08-12 21:36:50 -0400

    Week 7 - Exercise 5

--- week7-week7_05.Nicknames/src/Nicknames.java
+++ week7-week7_05.Nicknames/src/Nicknames.java
@@ -5,7 +5,14 @@ import java.util.Map;
 public class Nicknames {
 
     public static void main(String[] args) {
-        // Do the operations requested in the assignment here!
+        
+        HashMap<String, String> nickNames = new HashMap<String, String>();
+        
+        nickNames.put("matti", "mage");
+        nickNames.put("mikael", "mixu");
+        nickNames.put("arto", "arppa");
+        
+        String mikaelNickName = nickNames.get("mikael");
+        System.out.println(mikaelNickName);
     }
-
 }
