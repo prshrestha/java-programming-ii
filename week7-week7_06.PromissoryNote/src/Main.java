b2a7a2ff3c3b4f863caa486d5b4d940d8979e5ef
cpflyingduck <prashanta@Prashantas-MBP.home>
2015-08-12 21:54:33 -0400

    Week 7 - exercise 06

--- week7-week7_06.PromissoryNote/src/Main.java
+++ week7-week7_06.PromissoryNote/src/Main.java
@@ -1,5 +1,12 @@
+
 public class Main {
+
     public static void main(String[] args) {
         // Test your program here
+        PromissoryNote mattisNote = new PromissoryNote();
+        mattisNote.setLoan("Arto", 51.5);
+        mattisNote.setLoan("Arto", 10.5);
+
+        System.out.println(mattisNote.howMuchIsTheDebt("Arto"));
     }
 }
