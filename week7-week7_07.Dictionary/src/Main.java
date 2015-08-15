569efeb74dff903ed8178a8733808bc544e6682d
cpflyingduck <prashanta@Prashantas-MBP.home>
2015-08-15 14:41:09 -0400

    Week 7 - exercise
    Testing automated testing with string inputs to Scanner class.

--- week7-week7_07.Dictionary/src/Main.java
+++ week7-week7_07.Dictionary/src/Main.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+
+        /*Adding String input objects for automated testing*/
+        String input = "add\n" + "cheese\n" + "juusto\n"
+                + "add\n" + "bier\n" + "olut\n"
+                + "add\n" + "book\n" + "kirja\n"
+                + "add\n" + "computer\n" + "tietokone\n"
+                + "add\n" + "auto\n" + "car\n"
+                + "translate\n" + "auto\n"
+                + "quit\n";
+
+        Scanner reader = new Scanner(input);
         Dictionary dict = new Dictionary();
 
         TextUserInterface ui = new TextUserInterface(reader, dict);
