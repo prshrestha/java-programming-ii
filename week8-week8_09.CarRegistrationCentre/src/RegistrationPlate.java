1407ecb63225977852bf7a44ccf7572d0ed058e8
cpflyingduck <prashanta@Prashantas-MBP.home>
2015-08-16 14:45:47 -0400

    Week 8 - Exercise 9
    First Part

---
+++ week8-week8_09.CarRegistrationCentre/src/RegistrationPlate.java
@@ -0,0 +1,61 @@
+
+public class RegistrationPlate {
+    // don't change the code which is already given to you
+
+    // ATTENTION: the object variable types are final, meaning that their value cannot be changed!
+    private final String regCode;
+    private final String country;
+
+    public RegistrationPlate(String country, String regCode) {
+        this.regCode = regCode;
+        this.country = country;
+    }
+    
+    
+    public String getCountry(){
+        return this.country;
+    }
+    
+    public String getRegisterationCode(){
+        return this.regCode;
+    }
+
+    @Override
+    public String toString() {
+        return country + " " + regCode;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+
+        if (getClass() != object.getClass()) {
+            return false;
+        }
+
+        RegistrationPlate newPlate = (RegistrationPlate) object;
+
+        if (!this.country.equals(newPlate.getCountry())) {
+            return false;
+        }
+
+        if (this.regCode == null || !this.regCode.equals(newPlate.getRegisterationCode())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        if (this.regCode == null) {
+            return 7;
+        }
+
+        return this.country.hashCode() + this.regCode.hashCode();
+    }
+}
+
+
