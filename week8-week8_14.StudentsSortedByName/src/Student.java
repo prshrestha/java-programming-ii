12e11fe81d62fdb03eec5785667e551ed2b00b23
cpflyingduck <prashanta@Prashantas-MBP.home>
2015-08-23 18:36:29 -0400

    Week 8 - Exercise 14

---
+++ week8-week8_14.StudentsSortedByName/src/Student.java
@@ -0,0 +1,24 @@
+
+public class Student implements Comparable <Student> {
+
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        return this.name.compareToIgnoreCase(student.getName());
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
