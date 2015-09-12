14226d0ec222f901abf74476dc4e1fb325329d16
cpflyingduck <prashanta@Prashantas-MBP.home>
2015-09-12 16:27:30 -0400

    Week 10 - All assignments
    All assignment for week 10 downloaded

---
+++ week10-week10_27.TheFinnishRingingCentre/src/Bird.java
@@ -0,0 +1,21 @@
+
+public class Bird {
+
+    private String name;
+    private String latinName;
+    private int ringingYear;
+
+    public Bird(String name, String latinName, int ringingYear) {
+        this.name = name;
+        this.latinName = latinName;
+        this.ringingYear = ringingYear;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.latinName + " (" + this.ringingYear + ")";
+    }
+}
+
+
