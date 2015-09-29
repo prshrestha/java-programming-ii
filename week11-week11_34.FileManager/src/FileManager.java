aca17f0a4cbd6a93592fd46bd0fcbb82f3ca3bb0
cpflyingduck <prashanta@Prashantas-MBP.home>
2015-09-29 18:50:34 -0400

    Week 11 - Exercise 34

--- week11-week11_34.FileManager/src/FileManager.java
+++ week11-week11_34.FileManager/src/FileManager.java
@@ -9,13 +9,41 @@ import java.util.Scanner;
 
 public class FileManager {
 
+    private File file;
+    private Scanner reader;
+
     public List<String> read(String file) throws FileNotFoundException {
-        return null;
+        Scanner reader;
+        this.file = new File(file);
+        reader = new Scanner(this.file);
+
+        List<String> words = new ArrayList<String>();
+
+        if (this.file.exists()) {
+            while (reader.hasNextLine()) {
+                String s = reader.nextLine();
+                words.add(s);
+            }
+            return words;
+        } else {
+            return null;
+        }
     }
 
     public void save(String file, String text) throws IOException {
+        FileWriter writer;
+        writer = new FileWriter(file);
+        writer.write(text);
+        writer.close();
     }
 
     public void save(String file, List<String> texts) throws IOException {
+        FileWriter writer;
+        writer = new FileWriter(file);
+        for (String str : texts) {
+            writer.write(str);
+            writer.write("\n");
+        }
+        writer.close();
     }
 }
--- week11-week11_34.FileManager/src/Main.java
+++ week11-week11_34.FileManager/src/Main.java
@@ -1,12 +1,23 @@
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         FileManager t = new FileManager();
+        /*for (String line : t.read("src/testinput2.txt")) {
+         System.out.println(line);
+         }*/
+        /*t.save("src/testinput2.txt", "test");
+         t.save("src/test3.txt", "Programming is fun! Woho!");*/
 
-        for (String line : t.read("src/testinput1.txt")) {
-            System.out.println(line);
-        }
+        List<String> fruits = new ArrayList<String>();
+        fruits.add("banana");
+        fruits.add("apple");
+        fruits.add("watermelon");
+
+        t.save("src/TestFruits.txt", fruits);
     }
 }
