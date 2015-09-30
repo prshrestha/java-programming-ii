638eff68c0b0fa381845b11616cc26c84e09f1d6
cpflyingduck <prashanta@Prashantas-MBP.home>
2015-09-29 22:03:11 -0400

    Week 11 - Exercise 37

--- week11-week11_37.Survey/src/survey/UserInterface.java
+++ week11-week11_37.Survey/src/survey/UserInterface.java
@@ -10,9 +10,53 @@ public class UserInterface implements Runnable {
 
     @Override
     public void run() {
-        // Create your app here
+        this.frame = new JFrame("Survey");
+        this.frame.setPreferredSize(new Dimension(300, 300));
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        createComponents(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
     }
 
+    private void createComponents(Container container) {
+        BoxLayout layout = new BoxLayout(container, BoxLayout.Y_AXIS);
+        ButtonGroup radioButtons = new ButtonGroup();
+        
+        container.setLayout(layout);
+
+        JLabel text = new JLabel("Are you?");
+        container.add(text);
+
+        //create radio buttons
+        JCheckBox yes = new JCheckBox("Yes!");
+        JCheckBox no = new JCheckBox("No!");
+
+        //add radio buttons
+        radioButtons.add(yes);
+        radioButtons.add(no);
+        
+        //add radio buttons to the canvas
+        container.add(yes);
+        container.add(no);
+
+        JLabel reason = new JLabel("Why?");
+        container.add(reason);
+
+        JRadioButton noReason = new JRadioButton("No reason.");
+        JRadioButton funReason = new JRadioButton("Because it is fun!");
+
+        radioButtons.add(noReason);
+        radioButtons.add(funReason);
+        
+        container.add(noReason);
+        container.add(funReason);
+        
+        JButton done = new JButton("Done!");
+        container.add(done);
+    }
 
     public JFrame getFrame() {
         return frame;
