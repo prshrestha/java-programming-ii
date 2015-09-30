0afb204364604d56fa2eb0b35d6c970611414072
cpflyingduck <prashanta@Prashantas-MBP.home>
2015-09-29 21:18:42 -0400

    Week 11 - Exercise 15

--- week11-week11_35.TwoDirectionDictionary/src/dictionary/Main.java
+++ week11-week11_35.TwoDirectionDictionary/src/dictionary/Main.java
@@ -1,7 +1,37 @@
 package dictionary;
 
 public class Main {
+
     public static void main(String[] args) {
         // Test your dictionary here
+        /*MindfulDictionary dict = new MindfulDictionary();
+         dict.add("apina", "monkey");
+         dict.add("banaani", "banana");
+         dict.add("apina", "apfe");
+
+         System.out.println(dict.translate("apina"));
+         System.out.println(dict.translate("monkey"));
+         System.out.println(dict.translate("programming"));
+         System.out.println(dict.translate("banana"));*/
+
+        /*MindfulDictionary dict = new MindfulDictionary();
+         dict.add("apina", "monkey");
+         dict.add("banaani", "banana");
+         dict.add("ohjelmointi", "programming");
+         dict.remove("apina");
+         dict.remove("banana");
+
+         System.out.println(dict.translate("apina"));
+         System.out.println(dict.translate("monkey"));
+         System.out.println(dict.translate("banana"));
+         System.out.println(dict.translate("banaani"));
+         System.out.println(dict.translate("ohjelmointi"));*/
+        
+        MindfulDictionary dict = new MindfulDictionary("src/words.txt");
+        dict.load();
+
+        System.out.println(dict.translate("apina"));
+        System.out.println(dict.translate("ohjelmointi"));
+        System.out.println(dict.translate("alla oleva"));
     }
 }
---
+++ week11-week11_35.TwoDirectionDictionary/src/dictionary/MindfulDictionary.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dictionary;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ */
+public class MindfulDictionary {
+
+    private Map<String, String> dict = new HashMap<String, String>();
+    private String file;
+
+    public MindfulDictionary() {
+
+    }
+
+    public MindfulDictionary(String file) {
+        this.file = file;
+    }
+
+    //checks for the file name and creates dictionary based upon that
+    public boolean load() {
+
+        try {
+            File f = new File(this.file);
+
+            Scanner fileReader = new Scanner(f);
+
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                String[] parts = line.split(":");
+                this.dict.put(parts[0], parts[1]);
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    //saves file
+    public boolean save() {
+
+        FileWriter writer;
+        try {
+            writer = new FileWriter(file);
+            for (String key : this.dict.keySet()) {
+                writer.write(key + ":" + this.dict.get(key));
+                writer.write("\n");
+            }
+            writer.close();
+            return true;
+        } catch (IOException ex) {
+            return false;
+        }
+    }
+
+//add english word and finnish word to the Map
+    public void add(String word, String translation) {
+        if (!this.dict.containsKey(word)) {
+            this.dict.put(word, translation);
+        }
+    }
+
+    //implement dictionary search functionality
+    public String translate(String word) {
+
+        if (this.dict.containsKey(word)) {
+            return this.dict.get(word);
+        } else if (this.dict.containsValue(word)) {
+            for (String key : this.dict.keySet()) {
+                if (this.dict.get(key).equals(word));
+                return key;
+            }
+        }
+        return null;
+    }
+
+    //removes word from the dictionary
+    public void remove(String word) {
+        if (this.dict.containsKey(word)) {
+            this.dict.remove(word);
+        }
+        if (this.dict.containsValue(word)) {
+            List<String> keys = new ArrayList<String>();
+            for (String key : this.dict.keySet()) {
+                if (this.dict.get(key).equals(word)) {
+                    keys.add(key);
+                }
+            }
+            for (String k : keys) {
+                this.dict.remove(k);
+            }
+        }
+    }
+}
